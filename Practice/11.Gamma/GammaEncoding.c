93440c6ffbc1e095d98029b0ad7e79bfc1963f13
Yuchan Jeon <eukkchan03@gmail.com>
2024-04-16 17:11:14 +0900

    Create GammaEncoding.c

---
+++ Practice/11.Gamma/GammaEncoding.c
@@ -0,0 +1,73 @@
+#pragma warning(disable:4996);
+#define _CRT_SECURE_NO_WARINGS
+#include <stdio.h>
+#include <windows.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	// 영상1 불러오기
+	BITMAPFILEHEADER bmpFile1;
+	BITMAPINFOHEADER bmpInfo1;
+	FILE* inputFile1 = NULL;
+	inputFile1 = fopen("AICenterY.bmp", "rb");
+	fread(&bmpFile1, sizeof(BITMAPFILEHEADER), 1, inputFile1);
+	fread(&bmpInfo1, sizeof(BITMAPINFOHEADER), 1, inputFile1);
+
+	// 영상1 크기 정보
+	int width1 = bmpInfo1.biWidth;
+	int height1 = bmpInfo1.biHeight;
+	int size1 = bmpInfo1.biSizeImage;
+	int bitCnt1 = bmpInfo1.biBitCount;
+	int stride1 = (((bitCnt1 / 8) * width1) + 3 / 4 * 4);
+
+	// 인풋이미지1 선언
+	unsigned char* inputImg1 = NULL;
+	inputImg1 = (unsigned char*)calloc(size1, sizeof(unsigned char));
+	fread(inputImg1, sizeof(unsigned char), size1, inputFile1);
+
+	// 아웃풋이미지1 선언
+	unsigned char* outputImg1 = NULL;
+	outputImg1 = (unsigned char*)calloc(size1, sizeof(unsigned char));
+
+	// 함수 y1 : AICenterY
+	unsigned char* y1 = NULL;
+	y1 = (unsigned char*)calloc(size1, sizeof(unsigned char));
+	for (int j = 0; j < height1; j++)
+		for (int i = 0; i < width1; i++)
+			y1[j * width1 + i] = inputImg1[j * stride1 + 3 * i + 0];
+
+	// 함수 y2 : Gamma encoding
+	unsigned char* y2 = NULL;
+	y2 = (unsigned char*)calloc(size1, sizeof(unsigned char));
+
+	// 알고리즘
+	float gamma = 0.4;
+	for (int j = 0; j < height1; j++)
+		for (int i = 0; i < width1; i++)
+			y2[j * width1 + i] = (unsigned char)(255 * pow(y1[j * width1 + i] / 255.0, 1 / gamma));
+
+	// 아웃풋이미지1 result1 할당
+	for (int j = 0; j < height1; j++)
+		for (int i = 0; i < width1; i++) {
+			outputImg1[j * stride1 + 3 * i + 0] = y2[j * width1 + i];
+			outputImg1[j * stride1 + 3 * i + 1] = y2[j * width1 + i];
+			outputImg1[j * stride1 + 3 * i + 2] = y2[j * width1 + i];
+		}
+
+	// 아웃풋이미지 파일1
+	FILE* outputFile1 = fopen("output.bmp", "wb");
+	fwrite(&bmpFile1, sizeof(BITMAPFILEHEADER), 1, outputFile1);
+	fwrite(&bmpInfo1, sizeof(BITMAPINFOHEADER), 1, outputFile1);
+	fwrite(outputImg1, sizeof(unsigned char), size1, outputFile1);
+
+	// 메모리 할당 해제
+	free(inputImg1);
+	fclose(inputFile1);
+
+	free(y1);
+
+	free(outputImg1);
+	fclose(outputFile1);
+}
