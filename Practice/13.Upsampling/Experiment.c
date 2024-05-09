bf507d34d77dcd28ca203e1369982f487c8a50d7
Yuchan Jeon <eukkchan03@gmail.com>
2024-05-10 00:06:37 +0900

    Update Experiment.c

--- Practice/13.Upsampling/Experiment.c
+++ Practice/13.Upsampling/Experiment.c
@@ -1,5 +1,5 @@
-#pragma warning(disable:4996);
 #define _CRT_SECURE_NO_WARINGS
+#pragma warning(disable:4996);
 #include <stdio.h>
 #include <windows.h>
 #include <math.h>
@@ -7,6 +7,7 @@
 
 int main(int argc, char* argv[])
 {
+	// 영상1 불러오기 (Subsampling.bmp)
 	BITMAPFILEHEADER bmpFile1;
 	BITMAPINFOHEADER bmpInfo1;
 	FILE* inputFile1 = NULL;
@@ -14,6 +15,7 @@ int main(int argc, char* argv[])
 	fread(&bmpFile1, sizeof(BITMAPFILEHEADER), 1, inputFile1);
 	fread(&bmpInfo1, sizeof(BITMAPINFOHEADER), 1, inputFile1);
 
+	// 영상2 불러오기 (AICenterY.bmp)
 	BITMAPFILEHEADER bmpFile2;
 	BITMAPINFOHEADER bmpInfo2;
 	FILE* inputFile2 = NULL;
@@ -21,83 +23,120 @@ int main(int argc, char* argv[])
 	fread(&bmpFile2, sizeof(BITMAPFILEHEADER), 1, inputFile2);
 	fread(&bmpInfo2, sizeof(BITMAPINFOHEADER), 1, inputFile2);
 
-	int width = bmpInfo1.biWidth;
-	int height = bmpInfo1.biHeight;
-	int size = bmpInfo1.biSizeImage;
-	int bitCnt = bmpInfo1.biBitCount;
-	int stride = (((bitCnt / 8) * width) + 3 / 4 * 4);
+	// 영상1 크기 정보
+	int width1 = bmpInfo1.biWidth;
+	int height1 = bmpInfo1.biHeight;
+	int size1 = bmpInfo1.biSizeImage;
+	int bitCnt1 = bmpInfo1.biBitCount;
+	int stride1 = (((bitCnt1 / 8) * width1) + 3 / 4 * 4);
 
-	int width2 = bmpInfo2.biWidth;
-	int height2 = bmpInfo2.biHeight;
-	int size2 = bmpInfo2.biSizeImage;
-	int bitCnt2 = bmpInfo2.biBitCount;
-	int stride2 = (((bitCnt / 8) * width2) + 3 / 4 * 4);
-
+	// 인풋이미지1 선언
 	unsigned char* inputImg1 = NULL;
-	inputImg1 = (unsigned char*)calloc(size, sizeof(unsigned char));
-	fread(inputImg1, sizeof(unsigned char), size, inputFile1);
+	inputImg1 = (unsigned char*)calloc(size1, sizeof(unsigned char));
+	fread(inputImg1, sizeof(unsigned char), size1, inputFile1);
 
+	// 함수 y1 : Subsampling
+	unsigned char* y1 = NULL;
+	y1 = (unsigned char*)calloc(size1, sizeof(unsigned char));
+	for (int j = 0; j < height1; j++)
+		for (int i = 0; i < width1; i++)
+			y1[j * width1 + i] = inputImg1[j * stride1 + 3 * i + 0];
+
+	// Upsampling 크기
+	int ratio = 1; // 크기를 키울 배율 (2의 ratio제곱만큼 키움)
+	int width2 = bmpInfo1.biWidth << ratio;
+	int height2 = bmpInfo1.biHeight << ratio;
+	int stride2 = ((bitCnt1 / 8) * width2 + 3) / 4 * 4;
+	int size2 = stride2 * height2;
+
+	// 함수 result1 : Upsampling (크기 size2)
+	unsigned char* result1 = (unsigned char*)calloc(size2, sizeof(unsigned char));
+
+	//// Nearest Neighbor Interpolation
+	//for (int j = 0; j < height2; j++)
+	//	for (int i = 0; i < width2; i++) {
+	//		int origY = j / (1 << ratio);
+	//		int origX = i / (1 << ratio);
+
+	//		result1[j * width2 + i] = y1[origY * width1 + origX];
+	//	}
+
+	const int filter[4] = { -1, 9, 9, -1 };
+	const int filterSum = 16;
+
+	// N-tap Interpolation
+	for (int j = 0; j < height2; j++)
+		for (int i = 0; i < width2; i++)
+		{
+			float origY = j / (float)(1 << ratio);
+			float origX = i / (float)(1 << ratio);
+
+			int Value = 0;
+
+			for (int k = 0; k < 4; k++)
+				for (int l = 0; l < 4; l++)
+				{
+					int idxY = origY - 1 + k;
+					if (idxY < 0) idxY = 0;
+					if (idxY >= height1) idxY = height1 - 1;
+					int idxX = origX - 1 + l;
+					if (idxX < 0) idxX = 0;
+					if (idxX >= width1) idxX = width1 - 1;
+
+					Value += y1[idxY * width1 + idxX] * filter[k] * filter[l];
+				}
+
+			Value /= (filterSum * filterSum);
+			if (Value < 0) Value = 0;
+			if (Value > 255) Value = 255;
+
+			result1[j * width2 + i] = Value;
+		}
+
+	// 인풋이미지1 선언
 	unsigned char* inputImg2 = NULL;
 	inputImg2 = (unsigned char*)calloc(size2, sizeof(unsigned char));
 	fread(inputImg2, sizeof(unsigned char), size2, inputFile2);
 
-	unsigned char* y1 = (unsigned char*)calloc(size, sizeof(unsigned char));
-	unsigned char* y2 = (unsigned char*)calloc(size2, sizeof(unsigned char));
 
-	for (int j = 0; j < height; j++)
-		for (int i = 0; i < width; i++)
-			y1[j * width + i] = inputImg1[j * stride + 3 * i + 0];
-
+	// 함수 y2 : AICenterY
+	unsigned char* y2 = NULL;
+	y2 = (unsigned char*)calloc(size2, sizeof(unsigned char));
 	for (int j = 0; j < height2; j++)
 		for (int i = 0; i < width2; i++)
 			y2[j * width2 + i] = inputImg2[j * stride2 + 3 * i + 0];
 
-	int ratio = 1;
-	int width3 = bmpInfo1.biWidth << ratio;
-	int height3 = bmpInfo1.biHeight << ratio;
-	int stride3 = ((bitCnt / 8) * width3 + 3) / 4 * 4;
-	int size3 = stride3 * height3;
-	printf("w : %d(%d)\nh : %d\ns : %d\n\n", width, stride, height, size);
-	printf("w : %d(%d)\nh : %d\ns : %d\n\n", width2, stride2, height2, size2);
-	printf("w : %d(%d)\nh : %d\ns : %d\n\n", width3, stride3, height3, size3);
-
-	unsigned char* y3 = (unsigned char*)calloc(size3, sizeof(unsigned char));
-	for (int j = 0; j < height; j++)
-		for (int i = 0; i < width; i++)
-		{
-			y3[(2 * j) * width3 + (2 * i)] = y1[j * width + i];
-			y3[(2 * j) * width3 + (2 * i + 1)] = y1[j * width + i];
-			y3[(2 * j + 1) * width3 + (2 * i)] = y1[j * width + i];
-			y3[(2 * j + 1) * width3 + (2 * i + 1)] = y1[j * width + i];
-		}
-
 	double mse = 0, psnr;
-	for (int j = 0; j < height3; j++)
-		for (int i = 0; i < width3; i++)
-			mse += (double)((y3[j * width3 + i] - y2[j * width2 + i]) * (y3[j * width3 + i] - y2[j * width2 + i]));
-	mse /= (width3 * height3);
+	for (int j = 0; j < height2; j++)
+		for (int i = 0; i < width2; i++)
+			mse += (double)((result1[j * width2 + i] - y2[j * width2 + i]) * (result1[j * width2 + i] - y2[j * width2 + i]));
+	mse /= (width2 * height2);
 	psnr = mse != 0.0 ? 10.0 * log10(255 * 255 / mse) : 99.99;
 	printf("MSE = %.2lf\nPSNR = %.2lfdB\n\n", mse, psnr);
 
+	// 아웃풋이미지1 선언
 	unsigned char* outputImg1 = NULL;
-	outputImg1 = (unsigned char*)calloc(size3, sizeof(unsigned char));
+	outputImg1 = (unsigned char*)calloc(size2, sizeof(unsigned char));
 
-	for (int j = 0; j < height3; j++)
-		for (int i = 0; i < width3; i++) {
-			outputImg1[j * stride3 + 3 * i + 0] = y3[j * width3 + i];
-			outputImg1[j * stride3 + 3 * i + 1] = y3[j * width3 + i];
-			outputImg1[j * stride3 + 3 * i + 2] = y3[j * width3 + i];
+	// 아웃풋이미지1 result1 할당
+	for (int j = 0; j < height2; j++)
+		for (int i = 0; i < width2; i++) {
+			outputImg1[j * stride2 + 3 * i + 0] = result1[j * width2 + i];
+			outputImg1[j * stride2 + 3 * i + 1] = result1[j * width2 + i];
+			outputImg1[j * stride2 + 3 * i + 2] = result1[j * width2 + i];
 		}
 
-	FILE* outputFile1 = fopen("Nearest_Neighbor_Interpolation.bmp", "wb");
-	bmpInfo1.biWidth = width3;
-	bmpInfo1.biHeight = height3;
-	bmpInfo1.biSizeImage = size3;
+	// 아웃풋이미지 파일1
+	FILE* outputFile1 = fopen("Upsampling.bmp", "wb");
+	bmpInfo1.biWidth = width2;
+	bmpInfo1.biHeight = height2;
+	bmpInfo1.biSizeImage = size2;
 	bmpFile1.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPFILEHEADER) + size2;
 	fwrite(&bmpFile1, sizeof(BITMAPFILEHEADER), 1, outputFile1);
 	fwrite(&bmpInfo1, sizeof(BITMAPINFOHEADER), 1, outputFile1);
 	fwrite(outputImg1, sizeof(unsigned char), size2, outputFile1);
 
+	// 메모리 할당 해제
 	free(inputImg1);
 	fclose(inputFile1);
 	free(inputImg2);
@@ -105,7 +144,8 @@ int main(int argc, char* argv[])
 
 	free(y1);
 	free(y2);
-	free(y3);
+	free(result1);
+
 	free(outputImg1);
 	fclose(outputFile1);
 }
